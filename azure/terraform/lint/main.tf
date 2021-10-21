f3357195d378c3b00ca6404a59b21780920c5b9e
Jürgen Brüder <juergen.brueder@gmail.com>
2021-10-21 14:24:03 +0200

    fix: use correct managed_disk_type

--- azure/terraform/lint/main.tf
+++ azure/terraform/lint/main.tf
@@ -64,7 +64,7 @@ resource "azurerm_virtual_machine" "main" {
     name              = "terratestosdisk1"
     caching           = "ReadWrite"
     create_option     = "FromImage"
-    managed_disk_type = "Standard_LRSss"
+    managed_disk_type = "Standard_LRS"
   }
 
   os_profile {
